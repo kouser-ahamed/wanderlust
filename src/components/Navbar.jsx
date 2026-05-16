d584aa19d93a73d93aaa074e5c499d8580825317
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-05-16 07:56:40 +0600

    Add user authentication to Navbar with logout functionality and avatar display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
+"use client";   
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+
+    const { 
+        data: session, 
+        isPending, //loading state
+        error, //error object
+        refetch //refetch the session
+    } = authClient.useSession() 
+
+    const user = session?.user;
+    console.log("Session Data:", user);
+    
+    const handleLogout = async () => {
+      await authClient.signOut();
+    }
+
   return (
     <div className=" py-4 px-15 border-b-2 border-gray-200">
       <nav className="flex justify-between items-center">
@@ -32,12 +50,33 @@ const Navbar = () => {
           <li>
             <Link href="/profile">Profile</Link>
           </li>
+        { user ? 
+        
+        <> 
+        <li>
+          <Avatar>
+        <Avatar.Image alt={user?.name} src={user?.image} />
+        <Avatar.Fallback>{user?.name ? user.name.charAt(0) : 'U'}</Avatar.Fallback>
+      </Avatar>
+          </li>
+        <li>
+          <Button variant="danger" className={"rounded-none"} onClick={handleLogout}>
+         
+            Logout
+        
+          </Button>
+          </li>
+        </>
+        :
+        
+        <> 
           <li>
             <Link href="/login">Login</Link>
           </li>
           <li>
             <Link href="/signup">SignUp</Link>
           </li>
+        </>}
         </ul>
       </nav>
     </div>
